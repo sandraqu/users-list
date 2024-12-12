72c7543db1f66bbe037a9208bf66a9b58fd52d8a
Gray Qu <378074+sandraqu@users.noreply.github.com>
2024-12-11 19:52:59 -0500

    setup basic router structure

---
+++ src/components/Form/FormDetails.jsx
@@ -0,0 +1,9 @@
+const FormDetails = () => {
+  return (
+    <div>
+      <h1>FormDetails</h1>
+    </div>
+  );
+};
+
+export default FormDetails;
---
+++ src/components/Form/FormDob.jsx
@@ -0,0 +1,9 @@
+const FormDob = () => {
+  return (
+    <div>
+      <h1>FormDob</h1>
+    </div>
+  );
+};
+
+export default FormDob;
